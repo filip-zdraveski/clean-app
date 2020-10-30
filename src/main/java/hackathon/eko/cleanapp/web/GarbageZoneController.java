2229f9eb03124183b12034f1864135cf6a4cffa8
Filip Zdraveski <filip.zdraveski@yahoo.com>
2020-10-30 22:34:58 +0100

    Test method

--- src/main/java/hackathon/eko/cleanapp/web/GarbageZoneController.java
+++ src/main/java/hackathon/eko/cleanapp/web/GarbageZoneController.java
@@ -1,5 +1,6 @@
 package hackathon.eko.cleanapp.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GarbageZoneController {
 
     // TODO: Implement methods here
+    @GetMapping
+    public String getGarbageZones() {
+        return "Test";
+    }
 }
