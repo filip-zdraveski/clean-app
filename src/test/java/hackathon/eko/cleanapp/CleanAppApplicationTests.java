675c152c08aa17890da8415e8808e540f2461472
Filip Zdraveski <filip.zdraveski@yahoo.com>
2020-10-30 22:19:07 +0100

    Initial commit

---
+++ src/test/java/hackathon/eko/cleanapp/CleanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.eko.cleanapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
