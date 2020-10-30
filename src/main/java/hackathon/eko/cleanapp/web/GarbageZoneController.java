54c6920533cf5b3573bec1d747793e2e80d00a9c
Filip Zdraveski <filip.zdraveski@yahoo.com>
2020-10-30 22:23:59 +0100

    Structure modification

---
+++ src/main/java/hackathon/eko/cleanapp/web/GarbageZoneController.java
@@ -0,0 +1,11 @@
+package hackathon.eko.cleanapp.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/garbage-zones")
+public class GarbageZoneController {
+
+    // TODO: Implement methods here
+}
