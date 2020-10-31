697a07d944d5f74b1eca9bd287fd4bceffc948e7
aavramovic <antonio.avramovic@gmail.com>
2020-10-31 20:29:57 +0100

    help me

--- ng-clean-app/src/app/app.module.ts
+++ ng-clean-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainViewComponent } from './Components/Views/main-view/main-view.component';
 import { MenuBarComponent } from './Components/Views/menu-bar/menu-bar.component';
 import {MatButtonModule} from "@angular/material/button";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {MatButtonModule} from "@angular/material/button";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
